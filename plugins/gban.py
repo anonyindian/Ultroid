c964ceddc21a473616fac8d4145079d15a85422a
Anonymous Indian <73958752+anonyindian@users.noreply.github.com>
2021-02-25 15:43:19 +0530

    Update gban.py

--- plugins/gban.py
+++ plugins/gban.py
@@ -48,7 +48,7 @@ async def get_user_from_id(user, event):
     return user_obj
 
 
-@telebot.on(ChatAction)
+@ultroid_cmd(ChatAction)
 async def handler(tele):
     if tele.user_joined or tele.user_added:
         try:
